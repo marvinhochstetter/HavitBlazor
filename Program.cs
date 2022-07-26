1a60773e9a1a491b3b73649c4f438065683ccd13
marvinhochstetter <hochstetter.marvin@gmail.com>
2022-07-26 15:02:01 +0200

    Start

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
 
 namespace HavitBlazor
 {
@@ -19,6 +21,8 @@ namespace HavitBlazor
 
             builder.Services.AddScoped(sp => new HttpClient
                 { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
+            builder.Services.AddHxServices();
 
             await builder.Build().RunAsync();
         }
